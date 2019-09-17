4ed58902502521c873034e1d4348c8f347e7383b
Iain Tooley <iaintooley1@gmail.com>
2019-09-17 16:43:20 +0100

    setting up river class and test

---
+++ River.rb
@@ -0,0 +1,10 @@
+class River
+
+  attr_reader(:name, :fish_array)
+
+  def initialize(name,array_of_fish)
+    @name = name
+    @fish_array = array_of_fish
+  end
+
+end
---
+++ specs/river_spec.rb
@@ -0,0 +1,10 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../River")
+require_relative("../Fish")
+
+class TestRiver < MiniTest::Test
+
+
+
+end
