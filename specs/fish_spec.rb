108a9eb595dad007cf625d8484755951b5abb299
Iain Tooley <iaintooley1@gmail.com>
2019-09-17 16:28:04 +0100

    tested fish has name - test passed

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -5,7 +5,13 @@ require_relative("../Fish")
 class TestFish < MiniTest::Test
 
   def setup()
-    
+    @fish1 = Fish.new("Bob")
+    @fish2 = Fish.new("Kirsty")
+    @fish3 = Fish.new("Paul")
+  end
+
+  def test_fish_has_name
+    assert_equal("Bob", @fish1.name)
   end
 
 end
